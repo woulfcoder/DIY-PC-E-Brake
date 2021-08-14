c30fb3701cb379cab6012109dd05ffdd5841b066
woulfcoder <68704320+woulfcoder@users.noreply.github.com>
2021-08-14 15:09:11 +0200

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -1,18 +1,24 @@
-/*
- * 
- * 
- */
+//
+// 14-08-2021   Original version.
+//---------------------------------------------------
 
 #include <Joystick.h>
 
-int intAnalogRead;
-int intDeadzoneMin;
-int intDeadzoneMax;
+Joystick_ Joystick;
+
+#define analogIn A0
+
+int intDeadzoneMin = 515;
+int intDeadzoneMax = 850;
 
 void setup () {
+  Joystick.setBrakeRange(intDeadzoneMin, intDeadzoneMax);
+  
+  Joystick.begin();
 
+  pinMode(analogIn, INPUT);
 }
 
 void loop () {
-
+  
 }
