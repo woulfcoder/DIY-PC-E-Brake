103bce3795b8b3af6904f677e8fa93ab2512d027
woulfcoder <68704320+woulfcoder@users.noreply.github.com>
2021-08-14 17:48:19 +0200

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -18,15 +18,12 @@ void setup () {
   Joystick.setBrakeRange(intDeadzoneMin, intDeadzoneMax);
   
   Joystick.begin();
-  Serial.begin(9600);
 
   pinMode(analogIn, INPUT);
 }
 
 void loop () {
   intAnalogRead = analogRead(analogIn);
-
-  Serial.write(intAnalogRead);
 
   if (intAnalogRead > intDeadzoneMin && intAnalogRead < intDeadzoneMax) {
     Joystick.setBrake(intAnalogRead);
